7fddbf95db4039e8d60fb8da431b5ce708b4b099
Giuseppe Di Guglielmo <gusbertus@gmail.com>
2022-05-03 21:55:38 +0200

    added automated sitemap

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,48 @@
 <?php
 
 return [
-'debug' => true
+'debug' => true,
+
+'routes' => [
+    [
+        'pattern' => 'sitemap.xml',
+        'action'  => function() {
+            $pages = site()->pages()->index();
+            $ignores = kirby()->option('sitemap.ignore', ['error']);
+            // Allow ignore option to include children via 'page/*' syntax
+            $ignoresExpanded = [];
+
+            function endsWith($haystack, $needle) {
+                $length = strlen($needle);
+                return $length > 0 ? substr($haystack, -$length) === $needle : true;
+            }
+
+            function startsWith ( $haystack, $needle ) {
+                return strpos( $haystack , $needle ) === 0;
+              }
+            
+            foreach ($ignores as $ignore) {
+                if (endsWith($ignore, '/*')) {
+                $ignore = substr($ignore, 0, -2);
+                foreach ($pages as $page) {
+                    if (startsWith($page, $ignore)) {
+                    array_push($ignoresExpanded, $page);
+                    }
+                };
+                }
+            }
+            $ignore = array_merge($ignores, $ignoresExpanded);
+            $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+            return new Kirby\Cms\Response($content, 'application/xml');
+        }
+    ],
+    [
+        'pattern' => 'sitemap',
+        'action'  => function() {
+        return go('sitemap.xml', 301);
+        }
+    ]
+    ],
+    'sitemap.ignore' => ['error', 'Shop/*'],
 ];
